1fd06b92f7d53b83af50527aa5a5eb2b3763db0c
Will Boyd <shazzenbot@gmail.com>
2019-08-29 13:26:38 -0400

    add end state producer

--- src/main/java/com/linuxacademy/ccdak/clients/ProducerMain.java
+++ src/main/java/com/linuxacademy/ccdak/clients/ProducerMain.java
@@ -1,9 +1,39 @@
 package com.linuxacademy.ccdak.clients;
 
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
 public class ProducerMain {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        
+        props.put("acks", "all");
+        
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        
+        for (int i = 0; i < 100; i++) {
+            ProducerRecord record = new ProducerRecord<>("test_topic", 0, "count", Integer.toString(i));
+            producer.send(record, (RecordMetadata metadata, Exception e) -> {
+                if (e != null) {
+                    System.out.println("Error publishing message: " + e.getMessage());
+                } else {
+                    System.out.println("Published message: key=" + record.key() +
+                            ", value=" + record.value() +
+                            ", topic=" + metadata.topic() +
+                            ", partition=" + metadata.partition() +
+                            ", offset=" + metadata.offset());
+                }
+            });
+        }
+        
+        producer.close();
     }
 
 }
