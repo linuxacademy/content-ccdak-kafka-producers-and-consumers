ff63e39fbfbd7704d3f5c56f0432e863ad00e4c9
Will Boyd <shazzenbot@gmail.com>
2019-08-29 13:13:37 -0400

    init

---
+++ src/main/java/com/linuxacademy/ccdak/clients/ConsumerMain.java
@@ -0,0 +1,9 @@
+package com.linuxacademy.ccdak.clients;
+
+public class ConsumerMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
---
+++ src/main/java/com/linuxacademy/ccdak/clients/ProducerMain.java
@@ -0,0 +1,9 @@
+package com.linuxacademy.ccdak.clients;
+
+public class ProducerMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
